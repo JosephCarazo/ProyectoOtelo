cc12a99e885dd84a7981b5822f4718eb9984ea78
Joseph Carazo <jocarazope@est.utn.ac.cr>
2022-10-27 12:10:38 -0600

    Se crea la clase jugador

---
+++ src/Clases/Jugador.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author josep
+ */
+public class Jugador {
+   private String nombre;
+   private int turno;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+   
+   
+}
