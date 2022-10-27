89926750200ef3f3c955b9dd87963b81ac6126a8
Francisco Antonio Guzman Rocha <fran556556@gmail.com>
2022-10-27 12:16:21 -0600

    Se crea paquete Frm y se crea la clase MatrizBotones

---
+++ src/Clases/MatrizBotones.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Francisco
+ */
+public final class MatrizBotones {
+    private int fila=12;
+    private int colum=12;
+    private JButton [][] boton;
+
+    public MatrizBotones() {
+        crearMatriz();
+    }
+    
+    public void crearMatriz(){
+        int x=20;
+        int y=10;
+        boton=new JButton[fila][colum];
+        for (int i = 0; i <fila; i++) {
+            for (int j = 0; j <colum; j++) {
+                boton[i][j]=new JButton();
+                boton[i][j]=setBounds(x, y, 50, 50);
+                
+                
+            }
+        }
+    }
+}
